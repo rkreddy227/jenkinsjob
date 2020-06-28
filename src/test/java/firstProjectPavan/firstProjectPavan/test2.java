24f19ee119b1bbd5c51367f866f58e1bd6f51ad4
rkreddy227 <rkreddy227@gmail.com>
2020-06-28 16:40:59 -0400

    added test2

--- src/test/java/firstProjectPavan/firstProjectPavan/test2.java
+++ src/test/java/firstProjectPavan/firstProjectPavan/test2.java
@@ -11,7 +11,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
 import org.testng.annotations.Test;
-public class test {
+public class test2 {
 
 	
 	
