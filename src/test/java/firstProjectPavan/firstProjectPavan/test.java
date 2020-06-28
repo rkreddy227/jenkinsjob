71f38a6968ffae17b2d1b7ccdfd35c102f8568fd
rkreddy227 <rkreddy227@gmail.com>
2020-06-28 12:07:33 -0400

    First Commit

---
+++ src/test/java/firstProjectPavan/firstProjectPavan/test.java
@@ -0,0 +1,33 @@
+package firstProjectPavan.firstProjectPavan;
+import static org.testng.Assert.assertFalse;
+import static org.testng.Assert.assertTrue;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.annotations.Test;
+public class test {
+
+	
+	
+	
+   @Test
+	public void jobsearchOnAllegisGroupHomePageBasic() {
+		
+		 System.setProperty("webdriver.chrome.driver","C:\\chromedriver.exe");					
+	        WebDriver driver = new ChromeDriver();
+	        driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS) ;
+	        		
+	        String baseUrl = "https://www.aerotek.com";					
+	        driver.get(baseUrl);
+	        System.out.println(driver.getTitle());
+           driver.quit();
+	}
+	
+	
+}
